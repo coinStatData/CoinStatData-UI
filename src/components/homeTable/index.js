c24d60e8d6f1736e194f32076f84888d4363864e
danieljhkim <danieljhkim1@gmail.com>
2022-06-12 21:54:47 -0400

    build issue

--- src/components/homeTable/index.js
+++ src/components/homeTable/index.js
@@ -1,14 +1,63 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import UserContext from '../../context/userContext';
 import Table from 'react-bootstrap/Table'
-import { ICON_PATHS } from '../../util/constants/coins';
 import { useNavigate  } from "react-router-dom";
 import './styles.css';
 
+const ICON_PATHS = {
+  'matic-network': require('../../assets/matic-network.webp'),
+  'stellar': require('../../assets/stellar.webp'),
+  'bitcoin-cash': require('../../assets/bitcoin-cash.webp'),
+  'ftx-token': require('../../assets/ftx-token.webp'),
+  'monero': require('../../assets/monero.webp'),
+  'ethereum-classic': require('../../assets/ethereum-classic.webp'),
+  'flow': require('../../assets/flow.webp'),
+  'dai': require('../../assets/dai.webp'),
+  'cosmos': require('../../assets/cosmos.webp'),
+  'staked-ether': require('../../assets/staked-ether.webp'),
+  'tezos': require('../../assets/tezos.webp'),
+  'theta-fuel': require('../../assets/theta-fuel.webp'),
+  'hedera-hashgraph': require('../../assets/hedera-hashgraph.webp'),
+  'avalanche-2': require('../../assets/avalanche-2.webp'),
+  'near': require('../../assets/near.webp'),
+  'uniswap': require('../../assets/uniswap.webp'),
+  'shiba-inu': require('../../assets/shiba-inu.webp'),
+  'vechain': require('../../assets/vechain.webp'),
+  'leo-token': require('../../assets/leo-token.webp'),
+  'algorand': require('../../assets/algorand.webp'),
+  'crypto-com-chain': require('../../assets/crypto-com-chain.webp'),
+  'bitcoin': require('../../assets/bitcoin.webp'),
+  'ethereum': require('../../assets/ethereum.webp'),
+  'ripple': require('../../assets/ripple.webp'),
+  'solana': require('../../assets/solana.webp'),
+  'dogecoin': require('../../assets/dogecoin.webp'),
+  'tether': require('../../assets/tether.webp'),
+  'usd-coin': require('../../assets/usd-coin.webp'),
+  'compound-usd-coin': require('../../assets/compound-usd-coin.webp'),
+  'theta-token': require('../../assets/theta-token.webp'),
+  'compound-ether': require('../../assets/compound-ether.webp'),
+  'axie-infinity': require('../../assets/axie-infinity.webp'),
+  'true-usd': require('../../assets/true-usd.webp'),
+  'filecoin': require('../../assets/filecoin.webp'),
+  'decentraland': require('../../assets/decentraland.webp'),
+  'the-sandbox': require('../../assets/the-sandbox.webp'),
+  'apecoin': require('../../assets/apecoin.webp'),
+  'kucoin-shares': require('../../assets/kucoin-shares.webp'),
+  'internet-computer': require('../../assets/internet-computer.webp'),
+  'frax': require('../../assets/frax.webp'),
+  'binancecoin': require('../../assets/binancecoin.webp'),
+  'cardano': require('../../assets/cardano.webp'),
+  'binance-usd': require('../../assets/binance-usd.webp'),
+  'okb': require('../../assets/okb.webp'),
+  'polkadot': require('../../assets/polkadot.webp'),
+  'chain-2': require('../../assets/chain-2.webp'),
+  'wrapped-bitcoin': require('../../assets/wrapped-bitcoin.webp'),
+  'tron': require('../../assets/tron.webp'),
+  'flow': require('../../assets/flow.webp'),
+}
+
 function HomeTable(props) {
-  const { resp_g, coin_g, update_g } = useContext(UserContext);
-  const [tableD, setTableD] = useState();
-  const [isLambda, setIsLambda] = useState();
+  const { update_g } = useContext(UserContext);
   let navigate = useNavigate();
 
   const clickCoin = (coin) => {
@@ -67,7 +116,4 @@ function HomeTable(props) {
     </div>
   );
 }
-
-
-
 export default HomeTable;
