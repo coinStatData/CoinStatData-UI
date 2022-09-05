e419004735474e61e52e4183ef459ae3c2644202
danieljhkim <danieljhkim1@gmail.com>
2022-09-05 12:20:32 -0400

    implement username change for chat

--- src/components/chat/components/infoBar/infoBar.js
+++ src/components/chat/components/infoBar/infoBar.js
@@ -1,20 +1,38 @@
-import React from 'react';
-
-import onlineIcon from './icon/onlineIcon.png';
+import React, { useState } from 'react';
+import csdIcon from './icon/csdLogo2.png';
+import Join from '../join/join';
 // import closeIcon from '../../icons/closeIcon.png';
 
 import './styles.css';
 
-const InfoBar = ({ room }) => (
-  <div className="infoBar">
-    <div className="leftInnerContainer">
-      {/* <img className="onlineIcon" src={onlineIcon} alt="online icon" /> */}
-      <h3>Coin Chat!</h3>
-    </div>
-    <div className="rightInnerContainer">
-      <a href="/"></a>
-    </div>
-  </div>
-);
+const InfoBar = ({ room }) => {
+  const [showJoin, setShowJoin] = useState(false);
+
+  const toggleJoin = () => {
+    setShowJoin(!showJoin);
+  }
+
+  return (
+    <>
+      <div className="info-bar">
+        <div className="left-inner-cont">
+          <h3>Coin Chat!</h3>
+          {/* <img className="csd-icon" src={csdIcon} alt="CSD Logo" /> */}
+        </div>
+        <div className="right-inner-cont">
+          <button onClick={toggleJoin}>Sign-In</button>
+        </div>
+      </div>
+      <div className="join-cont">
+        {showJoin && 
+          <>
+            <Join toggleJoin={toggleJoin} />
+          </>
+        }
+      </div>
+    </>
+  )
+
+};
 
 export default InfoBar;
