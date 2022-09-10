5d39cc84e5a702cd8412efeb4f061814f96d623d
danieljhkim <danieljhkim1@gmail.com>
2022-09-10 17:06:16 -0400

    env var

--- src/services/topTrending.service.js
+++ src/services/topTrending.service.js
@@ -3,7 +3,7 @@ import RestService from "./wrapper/restService";
 const topTrendingService = () => {
   
   const _serverUr = process.env.REACT_APP_NODE_ENV === "dev" ? 
-    process.env.REACT_APP_DEV_SERVER_URL : process.env.REACT_APP_SERVER_URL;
+    process.env.REACT_APP_DEV_SERVER_URL : process.env.SERVER_URL;
 
   const _fetchCoins = async () => {
     try {
