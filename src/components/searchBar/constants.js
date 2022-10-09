c4145305bb6fcb2e1d5e4c4886e95194327b6606
danieljhkim <danieljhkim1@gmail.com>
2022-10-09 11:25:57 -0400

    implement error alert modal

---
+++ src/components/searchBar/constants.js
@@ -0,0 +1,5 @@
+
+const INPUT_ERROR_MSG = "We could not find the coin. Please try another!";
+const NETWORK_ERROR_MSG = "Oopse, something went wrong. Please try again later!";
+
+module.exports = { INPUT_ERROR_MSG, NETWORK_ERROR_MSG }
