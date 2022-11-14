1bf8c8005da7ee979c5c36f96b72d1eab4151009
danieljhkim <danieljhkim1@gmail.com>
2022-11-14 16:50:11 -0500

    resolve build error

--- src/util/constants/coins.js
+++ src/util/constants/coins.js
@@ -4,10 +4,35 @@ const COIN_LIST = COIN_STR.split(",").sort();
 
 const CSD_STR = "bitcoin,ethereum,dash,aptos,zcash,ripple,solana,dogecoin,theta-token,axie-infinity,filecoin,decentraland,the-sandbox,apecoin,kucoin-shares,internet-computer,maker,binancecoin,quant-network,cardano,okb,polkadot,chain-2,wrapped-bitcoin,tron,flow,cosmos,tezos,theta-fuel,hedera-hashgraph,avalanche-2,near,uniswap,shiba-inu,vechain,leo-token,algorand,matic-network,stellar,bitcoin-cash,monero,ethereum-classic,eos,klay-token,pancakeswap-token,neo,helium,evmos,the-graph,fantom";
 
-const CSD_LIST = CSD_STR.split(',');
+const CSD_LIST = [
+  'bitcoin',           'ethereum',      'dash',
+  'aptos',             'zcash',         'ripple',
+  'solana',            'dogecoin',      'theta-token',
+  'axie-infinity',     'filecoin',      'decentraland',
+  'the-sandbox',       'apecoin',       'kucoin-shares',
+  'internet-computer', 'maker',         'binancecoin',
+  'quant-network',     'cardano',       'okb',
+  'polkadot',          'chain-2',       'wrapped-bitcoin',
+  'tron',              'flow',          'cosmos',
+  'tezos',             'theta-fuel',    'hedera-hashgraph',
+  'avalanche-2',       'near',          'uniswap',
+  'shiba-inu',         'vechain',       'leo-token',
+  'algorand',          'matic-network', 'stellar',
+  'bitcoin-cash',      'monero',        'ethereum-classic',
+  'eos',               'klay-token',    'pancakeswap-token',
+  'neo',               'helium',        'evmos',
+  'the-graph',         'fantom'
+];
 
 const STABLE_LIST = ["tether", "usd-coin","binance-usd","dai","frax","true-usd","usdd","gemini-dollar","paxos-standard","compound-usd-coin"];
 
-const CSD_INDEX = [ ...CSD_LIST, ...STABLE_LIST];
+const CSD_INDEX = [
+  'bitcoin', 'ethereum', 'dash', 'aptos', 'zcash', 'ripple', 'solana', 'dogecoin', 'theta-token', 'axie-infinity', 'filecoin', 'decentraland',
+  'the-sandbox', 'apecoin', 'kucoin-shares', 'internet-computer', 'maker', 'binancecoin', 'quant-network', 'cardano', 'okb',
+  'polkadot', 'chain-2', 'wrapped-bitcoin', 'tron', 'flow', 'cosmos', 'tezos', 'theta-fuel', 'hedera-hashgraph','avalanche-2', 'near', 'uniswap',
+  'shiba-inu', 'vechain', 'leo-token', 'algorand', 'matic-network', 'stellar', 'bitcoin-cash', 'monero', 'ethereum-classic',
+  'eos', 'klay-token', 'pancakeswap-token', 'neo', 'helium', 'evmos', 'the-graph', 'fantom', 'tether', 'usd-coin', 'binance-usd', 'dai',
+  'frax', 'true-usd', 'usdd', 'gemini-dollar', 'paxos-standard', 'compound-usd-coin'
+]
 
-module.exports = { COIN_LIST, COIN_STR, STABLE_LIST, CSD_INDEX, CSD_LIST, CSD_STR };
+module.exports = { COIN_LIST, COIN_STR, STABLE_LIST, CSD_LIST, CSD_STR, CSD_INDEX };
