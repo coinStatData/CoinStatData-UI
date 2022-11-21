33c64fcbf01cd2e0c7e0e17762f546afc6625300
danieljhkim <danieljhkim1@gmail.com>
2022-11-20 19:53:40 -0500

    update cursor

--- src/containers/marquee/index.js
+++ src/containers/marquee/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchMarquee } from '../../redux/actions/marquee';
 import Entry from './entry';
